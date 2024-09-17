5e20244d346dc24d928bb47ea7170c800595e262
DoHunee <jehgns1@egmail.com>
2024-09-17 15:10:44 +0900

    스프링 부트 - 오류 페이지

--- src/main/java/hello/exception/WebServerCustomizer.java
+++ src/main/java/hello/exception/WebServerCustomizer.java
@@ -10,9 +10,9 @@ import org.springframework.stereotype.Component;
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
     @Override
     public void customize(ConfigurableWebServerFactory factory) {
-        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/errorpage/404");
-        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR,"/error-page/500");
-        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/errorpage/404");             // response.sendError(404) : errorPage404 호출
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR,"/error-page/500"); // response.sendError(500) : errorPage500 호출
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500"); // RuntimeException 또는 그 자식 타입의 예외: errorPageEx 호출
         factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
     }
 }
