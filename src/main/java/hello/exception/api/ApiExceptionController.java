8d6bde0b6e9adc265b04a0debb623f63253c2784
DoHunee <jehgns1@egmail.com>
2024-09-18 10:02:20 +0900

    API 예외 처리 - 시작

---
+++ src/main/java/hello/exception/api/ApiExceptionController.java
@@ -0,0 +1,32 @@
+package hello.exception.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class ApiExceptionController {
+
+    // http://localhost:8080/api/members/spring
+    // http://localhost:8080/api/members/ex => 예외처리
+    // postman으로 검증! => header에 accept 타입을 application/json으로 설정해야 한다.
+    @GetMapping("/api/members/{id}")
+    public MemberDto getMember(@PathVariable("id") String id) {
+        if (id.equals("ex")) {
+            throw new RuntimeException("잘못된 사용자");
+        }
+        return new MemberDto(id, "hello " + id);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String memberId;
+        private String name;
+    }
+}
